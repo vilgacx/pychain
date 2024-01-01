c155d405b53c3cc1760199bc6cc5100c845a75c0
xorvet <cypherx702@gmail.com>
2024-01-01 23:02:53 +0530

    validate test

--- src/test.py
+++ src/test.py
@@ -2,7 +2,15 @@ from pychain import pyChain
 
 init = pyChain()
 
+# mining
 init.Mine(1234,"hello world")
 init.Mine(8987,"tachyon")
 
 print(init.chain)
+
+# validate
+print(init.Validate())
+
+# tampering
+init.chain[1]["data"] = "py"
+print(init.Validate())

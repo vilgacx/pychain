b6939f008b672dc1611f59228b6f6127703a7ef8
xorvet <cypherx702@gmail.com>
2024-01-01 22:32:33 +0530

    mining update + todigest

--- src/pychain.py
+++ src/pychain.py
@@ -6,8 +6,9 @@ class pyChain:
     def __init__(self):
         self.chain = [] 
         self.nodes = []
+        self.chain_length = 0
         self.difficulty = max(3,min((len(self.nodes)+1)//3,7));
-        self.chain.append(self.Block(0,"0"*64,None,0,0,"genesis"))
+        self.Mine(0,"genesis")
 
     def Block(self, index, hash, prev_hash, pow, nonce, data):
         return {
@@ -23,23 +24,29 @@ class pyChain:
         }
 
     def Mine(self, nonce, data):
-        prev_block = self.chain[-1]
+        block = {}
+        if self.chain_length > 0:
+            prev_block = self.chain[-1]
 
-        index = len(self.chain)
-        pow = self.PoW(index,prev_block["pow"],nonce,data)
-        prev_hash = prev_block["hash"]
+            index = len(self.chain)
+            pow = self.PoW(index,prev_block["pow"],nonce,data)
+            prev_hash = prev_block["hash"]
 
-        block = self.Block(index,"",prev_hash, pow, nonce, data)
-        block["hash"] = self.Hash(block);
+            block = self.Block(index,"",prev_hash, pow, nonce, data)
+            block["hash"] = self.Hash(block);
+        else:
+            block = self.Block(0, "", None, 0, nonce, data)
+            block["hash"] = self.Hash(block)
 
         self.chain.append(block)
-    
+        self.chain_length += 1
+
     def PoW(self, index, prev_pow, nonce, data):
         pow = 1
         solve = "0"*self.difficulty;
 
         while True:
-            digest = (str((pow+prev_pow+nonce**2)+index)+data).encode()
+            digest = self.toDigest(index,pow,prev_pow,nonce,data)
             hashval = hashlib.sha256(digest).hexdigest()
 
             if hashval[:self.difficulty] == solve:
@@ -48,7 +55,9 @@ class pyChain:
                 pow += 1
 
         return pow
-
+ 
+    def toDigest(self,index,pow,prev_pow,nonce,data):
+        return (str((pow+prev_pow+nonce**2)+index)+data).encode()
 
     def Hash(self,val):
         hash = hashlib.sha256(json.dumps(val, sort_keys=True).encode()).hexdigest()
